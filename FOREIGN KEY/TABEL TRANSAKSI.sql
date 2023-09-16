246df8cac8eab29235bac560fc641de6568332bc
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-09-16 19:14:52 +0700

    Delete FOREIGN KEY/TABEL TRANSAKSI.sql

--- FOREIGN KEY/TABEL TRANSAKSI.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE  transaksi (
-id_transaksi INT PRIMARY KEY AUTO_INCREMENT,
-jumlah_transaksi DECIMAL(5,5),
-id_customer INT,
-FOREIGN KEY(id_customer) REFERENCES customer(id_customer)
-);
-
-SELECT * FROM transaksi;
-        
