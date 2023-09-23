a32677ab225e4bd6b4ecb8dd1e4615c01cd8826f
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-09-23 14:40:49 +0700

    Add files via upload

---
+++ UNIQUE/VALUES UNIQUE TABLE PRODUK.sql
@@ -0,0 +1,12 @@
+INSERT INTO produk (id_produk, nama_produk, harga)
+VALUES
+    (1, 'Product A', 10.99),
+    (2, 'Product B', 15.50),
+    (3, 'Product C', 8.75),
+    (4, 'Product D', 12.25),
+    (5, 'Product E', 9.99),
+    (6, 'Product F', 14.75),
+    (7, 'Product G', 11.50),
+    (8, 'Product H', 7.25),
+    (9, 'Product I', 6.99),
+    (10, 'Product J', 13.25);
