f4624b6cfcd00e71f0970a8d862383516aa69c22
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-11-05 23:15:42 +0700

    Delete STORED PROCEDURE.sql

--- STORED PROCEDURE.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE GetDistinctNames()
-BEGIN
-    SELECT DISTINCT first_name, last_name
-    FROM customer
-    INNER JOIN transaksi ON transaksi.id_transaksi = customer.id_customer;
-END;
-//
-DELIMITER ;
