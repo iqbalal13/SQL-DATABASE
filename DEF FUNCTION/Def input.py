b1927f4c2a32118c378a38b4e9c4bf63af6330d8
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-11-14 23:04:00 +0700

    Add files via upload

---
+++ DEF FUNCTION/Def input.py
@@ -0,0 +1,7 @@
+def greet():
+    """This function takes user input for a name and prints a greeting."""
+    name = input("Enter your name: ")
+    print(f"Hello, {name}!")
+
+# Call the function
+greet()
