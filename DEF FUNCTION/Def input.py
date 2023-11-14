9036db2f836761b4a55abac0476d96798e91fa9e
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-11-14 23:04:30 +0700

    Delete DEF FUNCTION/Def input.py

--- DEF FUNCTION/Def input.py
+++
@@ -1,7 +0,0 @@
-def greet():
-    """This function takes user input for a name and prints a greeting."""
-    name = input("Enter your name: ")
-    print(f"Hello, {name}!")
-
-# Call the function
-greet()
